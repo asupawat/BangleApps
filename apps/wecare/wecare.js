e013e8da62352dfec94f7bb9557c90d7a9df9ad5
Noom <asupawat@users.noreply.github.com>
2021-09-12 11:24:05 +0700

    Create wecare.js

---
+++ apps/wecare/wecare.js
@@ -0,0 +1,63 @@
+var FACES = [];
+var STOR = require("Storage");
+STOR.list(/\.face\.js$/).forEach(face=>FACES.push(eval(require("Storage").read(face))));
+var lastface = STOR.readJSON("multiclock.json")||{pinned:0};
+var iface = lastface.pinned;
+var face = FACES[iface]();
+var intervalRefSec;
+
+//if (STOR.read("fnt36x70.js")) eval(STOR.read("fnt36x70.js"));
+
+function stopdraw() {
+  if (face.kill) face.kill();
+  if(intervalRefSec) {intervalRefSec=clearInterval(intervalRefSec);}
+  g.clear();
+}
+
+function startdraw() {
+  g.clear();
+  g.reset();
+  Bangle.drawWidgets();
+  face.init();
+  if (face.tick) intervalRefSec = setInterval(face.tick,1000);
+}
+
+global.SCREENACCESS = {
+  withApp:true,
+  request:function(){
+    this.withApp=false;
+    stopdraw();
+  },
+  release:function(){
+    this.withApp=true;
+    startdraw(); 
+  }
+}; 
+
+function setControl(){
+  function newFace(inc){
+    var n = FACES.length-1;
+    iface+=inc;
+    iface = iface>n?0:iface<0?n:iface;
+    stopdraw();
+    face = FACES[iface]();
+    startdraw();
+  }
+  function finish(){
+    if (lastface.pinned!=iface){
+        lastface.pinned=iface;
+        STOR.write("multiclock.json",lastface);
+    }
+    Bangle.showLauncher();
+  }
+  Bangle.on('swipe',(dir)=>{
+    if (SCREENACCESS.withApp)
+      newFace(dir);
+  });
+  setWatch(finish,BTN1,{edge:"falling"});
+}
+
+Bangle.loadWidgets();
+g.clear();
+startdraw();
+setControl();
